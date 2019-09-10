3337ff92fba2fa811a5436664e0ac5dfb5da5c28
radville <laura.radville@gmail.com>
2019-09-10 13:59:27 -0400

    only current user can edit and delete sightings

--- app/controllers/bird_sighting_controller.rb
+++ app/controllers/bird_sighting_controller.rb
@@ -10,13 +10,16 @@ class BirdSightingsController < ApplicationController
     end
     
     post "/sightings/" do
-        puts params
-        sighting = BirdSighting.create(common_name: params[:common_name], scientific_name: params[:scientific_name], \
-            datetime: params[:datetime], location: params[:location], description: params[:description], \
-            credit: params[:credit], img_src: params[:img_src], license_url: params[:license_url], \
-            order: params[:order], family: params[:family], user_id: session[:user_id])
-    
-        redirect to "/sightings/#{sighting.id}"
+        if logged_in?
+            sighting = BirdSighting.create(common_name: params[:common_name], scientific_name: params[:scientific_name], \
+                datetime: params[:datetime], location: params[:location], description: params[:description], \
+                credit: params[:credit], img_src: params[:img_src], license_url: params[:license_url], \
+                order: params[:order], family: params[:family], user_id: session[:user_id])
+        
+            redirect to "/sightings/#{sighting.id}"
+        else
+            redirect to "/users/login"
+        end
     end
 
     get "/sightings/new" do
@@ -48,30 +51,42 @@ class BirdSightingsController < ApplicationController
     get "/sightings/:id/edit" do
         if logged_in?
             @sighting = BirdSighting.find_by_id(params[:id])
-    
-            erb :"bird_sightings/edit"
+            if @sighting && sighting.user == current_user
+                erb :"bird_sightings/edit"
+            else
+                redirect to "/sightings/"
+            end
         else
             redirect to "/users/login"
         end
     end
     
     patch "/sightings/:id" do
-        sighting = BirdSighting.find_by_id(params[:id])
-        
-        sighting.update(common_name: params[:common_name]) if params[:common_name] != "" 
-        sighting.update(scientific_name: params[:scientific_name]) if params[:scientific_name] != "" 
-        sighting.update(datetime: params[:datetime]) if params[:datetime] != "" 
-        sighting.update(location: params[:location]) if params[:location] != "" 
-        sighting.update(description: params[:description]) if params[:description] != "" 
+        if logged_in?
+            sighting = BirdSighting.find_by_id(params[:id])
+            if sighting && sighting.user == current_user
+                sighting.update(common_name: params[:common_name]) if params[:common_name] != "" 
+                sighting.update(scientific_name: params[:scientific_name]) if params[:scientific_name] != "" 
+                sighting.update(datetime: params[:datetime]) if params[:datetime] != "" 
+                sighting.update(location: params[:location]) if params[:location] != "" 
+                sighting.update(description: params[:description]) if params[:description] != "" 
     
-        redirect to "/sightings/#{sighting.id}"
+                redirect to "/sightings/#{sighting.id}"
+            else
+                redirect to "/sightings/"
+            end
     end
     
     delete "/sightings/:id/delete" do
-        sighting = BirdSighting.find_by_id(params[:id])
-        sighting.delete
-
-        redirect to "/sightings/"
+        if logged_in?
+            sighting = BirdSighting.find_by_id(params[:id])
+            if sighting && sighting.user == current_user
+                sighting.delete
+            end
+            redirect to "/sightings/"
+        else
+            redirect to "users/login"
+        end
     end
 
 end
